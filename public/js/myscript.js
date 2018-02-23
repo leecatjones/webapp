b3aecb41884735affadfc6af7007f7310e886056
10student <10student@nr.stu>
2018-02-22 20:35:53 -0500

    Added layout and styles

---
+++ public/js/myscript.js
@@ -0,0 +1,17 @@
+//var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+//url += "?" + $.param({
+//  "api-key": "3ce00a9e38074869a53f1314913e7d29"
+// });$ajax({
+//  url: url,
+//  method: "GET",
+
+// }).done(function(result) {
+//  console.log(result);
+// }).fail(function(err) {
+//  throw err;
+// });
+//
+var xhttp = new XMLHttpRequest();
+xttp.onreadystatechange = function() {
+  if (this.readyState == 4 && )
+}
